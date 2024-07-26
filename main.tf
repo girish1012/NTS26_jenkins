197ca45f449456e29a8faf50c1d692a55cd51d68
girish1012 <girish8012@gmail.com>
2024-07-26 09:55:46 +0000

    commit5

--- main.tf
+++ main.tf
@@ -11,11 +11,4 @@ resource "aws_instance" "tomcat-server" {
     "Name"="Tomcat_Server"
   }
 
-resource "aws_instance" "tomcat-server" {
-  ami = "ami-0ad21ae1d0696ad58"
-  instance_type = "t2.small"
-  key_name = "ssh_2024_May"
-  tags={
 
-    "Name"="Tomcat_Server"
-  }
